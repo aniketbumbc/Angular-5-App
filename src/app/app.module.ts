805d9a1bba9fbfed74b51353df23b068caae94c6
aniket umbc <aniket.umbc@gmail.com>
2017-12-28 09:09:28 +0530

    add user list-47

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
