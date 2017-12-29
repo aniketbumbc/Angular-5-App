f4aae79b1ee8d2635fbedc3f97ef0124722edb15
aniket umbc <aniket.umbc@gmail.com>
2017-12-29 08:13:03 +0530

    add input decorator

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  name: string;
-  constructor() {
-    this.name = 'Aniket';
+  @Input () name: string; // declare name as input ( decorator )
+    constructor() {
   }
 
   ngOnInit() {
